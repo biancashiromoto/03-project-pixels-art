817f8420ef1b195c09b3a542fc78e70da4ca7935
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2023-02-06 19:32:10 -0300

    [Bianca Shiromoto] - Remove colors.js, add requirements 2 and 3

--- colors.js
+++
@@ -1,9 +0,0 @@
-const colors = {
-  black: '#000000',
-  orange: '#ffb86c',
-  cyan: '#8be9fd',
-  green: '#50fa7b',
-  // red: '#ff5555',
-  // pink: '#ff79c6',
-  // yellow: '#f1fa8c'
-};
--- script.js
+++ script.js
@@ -1,5 +1,20 @@
-window.onload = () => {
-  const title = document.getElementsByTagName("h1")[0];
-  title.innerHTML = "Paleta de Cores";
-  title.setAttribute("id", "title");
-};
+const colors = ['#000000', '#ffb86c', '#8be9fd', '#50fa7b', '#ff5555', '#ff79c6', '#f1fa8c'];
+
+// window.onload = () => {
+const title = document.getElementsByTagName('h1')[0];
+title.innerHTML = 'Paleta de Cores';
+title.setAttribute('id', 'title');
+// };
+const main = document.getElementsByTagName('main')[0];
+const colorPalette = document.createElement('section');
+colorPalette.setAttribute('id', 'color-palette');
+colorPalette.style.display = 'flex';
+main.appendChild(colorPalette);
+for (let index = 0; index < colors.length; index += 1) {
+  const colorSample = document.createElement('div');
+  colorSample.style.backgroundColor = colors[index];
+  colorSample.classList.add('color');
+  colorSample.style.border = 'solid 1px black';
+  colorPalette.appendChild(colorSample);
+  console.log(colorSample);
+}
