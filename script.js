6b5dec8ae541cfd42eca08c563aee636e5de814a
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2023-02-07 13:12:56 -0300

    [Bianca Shiromoto] - Start requirement 5, finish requirement 2

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
-const colors = ['#000000', '#ffb86c', '#8be9fd', '#50fa7b', '#ff5555', '#ff79c6', '#f1fa8c'];
+const colors = ['#000000', '#ffb86c', '#8be9fd', '#50fa7b'];
 
 const title = document.getElementsByTagName('h1')[0];
 title.innerHTML = 'Paleta de Cores';
 title.setAttribute('id', 'title');
 const main = document.getElementsByTagName('main')[0];
+const articlePalette = document.createElement('article');
+main.appendChild(articlePalette);
+articlePalette.style.display = 'flex';
 const colorPalette = document.createElement('section');
 colorPalette.setAttribute('id', 'color-palette');
+colorPalette.style.width = '500px';
+colorPalette.style.height = '100px';
 colorPalette.style.display = 'flex';
-main.appendChild(colorPalette);
+articlePalette.appendChild(colorPalette);
 for (let index = 0; index < colors.length; index += 1) {
   const colorSample = document.createElement('div');
   colorSample.style.backgroundColor = colors[index];
@@ -29,14 +34,16 @@ const calcRandomColor = () => {
 const colorSquare = document.querySelectorAll('.color');
 const changeBackgroundColor = () => {
   const bcgkColorArray = [];
-  for (let index = 1; index < colorSquare.length; index += 1) {
+  localStorage.setItem('colorPalette', bcgkColorArray);
+  for (let index = 0; index < colorSquare.length; index += 1) {
+    colorSquare[0].style.backgroundColor = '#000000';
     const colorSquareItem = colorSquare[index];
-    if (colorSquareItem.style.backgroundColor !== '#000000') {
-      colorSquareItem.style.backgroundColor = calcRandomColor();
-      bcgkColorArray.push(colorSquareItem.style.backgroundColor);
-    }
+    colorSquareItem.style.backgroundColor = calcRandomColor();
+    bcgkColorArray.push(colorSquareItem.style.backgroundColor);
   }
+  localStorage.colorPalette = JSON.stringify(bcgkColorArray);
 };
+
 randomColors.addEventListener('click', () => {
   changeBackgroundColor();
 });
