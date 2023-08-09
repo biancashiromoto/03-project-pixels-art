9cdfeebb90b9560421db5d5d795d1094cbfa0fc9
biancashiromoto <b.shiromoto.bs@gmail.com>
2023-08-09 18:23:01 -0300

    feat: Create ColorPalette component

---
+++ src/components/ColorPalette/ColorPalette.jsx
@@ -0,0 +1,20 @@
+import { useContext } from 'react';
+import { context } from '../../context/context';
+import Pixel from '../Pixel/Pixel';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './ColorPalette.css';
+
+const ColorPalette = () => {
+  const { colors } = useContext(context);
+  return (
+    <div className='d-flex mx-2'>
+      {colors.map((color, index) => (
+        <div key={index} className='pixel-container d-flex'>
+            <Pixel props={color} />
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default ColorPalette;
