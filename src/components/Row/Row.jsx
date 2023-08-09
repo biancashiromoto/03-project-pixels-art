f362139412ffce9f2d733a430a1e2054b7354830
biancashiromoto <b.shiromoto.bs@gmail.com>
2023-08-09 19:06:33 -0300

    feat: Add paintPixel function

--- src/components/Row/Row.jsx
+++ src/components/Row/Row.jsx
@@ -4,10 +4,22 @@ import Pixel from '../Pixel/Pixel';
 
 const Row = () => {
   const { currColor, pixels } = useContext(context);
+
+  const paintPixel = (e) => {
+    e.target.style.backgroundColor = currColor;
+  }
+
   return (
     <div className='d-flex'>
       {pixels.map((_pixel, index) => (
-        <Pixel key={index} />
+        <Pixel
+          key={index}
+          onClick={ (e) => {
+            paintPixel(e);
+            console.log(`a pixel from Row was clicked and should have ${currColor} color`);
+          } }
+
+        />
       ))}
     </div>
   )
