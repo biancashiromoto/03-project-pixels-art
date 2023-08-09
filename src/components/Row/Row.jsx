5ba03df1781ccce04054b7383a4c6d35b8d9dc61
biancashiromoto <b.shiromoto.bs@gmail.com>
2023-08-09 20:00:42 -0300

    fix: Fix backgroundColor double click toggle

--- src/components/Row/Row.jsx
+++ src/components/Row/Row.jsx
@@ -1,21 +1,21 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { context } from "../../context/context";
 import Pixel from '../Pixel/Pixel';
-import './Row.module.css';
 
 const Row = () => {
   const { currColor, pixels } = useContext(context);
 
   const paintPixel = (e) => {
     let pixelStyle = e.target.style;
+    console.log(pixelStyle.backgroundColor);
     const white = 'rgb(255, 255, 255)';
-    (pixelStyle.backgroundColor === white)
-      ? (pixelStyle.backgroundColor = currColor)
-      : (pixelStyle.backgroundColor = white);
+    (pixelStyle.backgroundColor === currColor)
+      ? (pixelStyle.backgroundColor = white)
+      : (pixelStyle.backgroundColor = currColor);
   }
 
   return (
-    <div className='pixel-container d-flex'>
+    <div className={ `d-flex pixel-container}` }>
       {pixels.map((_pixel, index) => (
         <Pixel
           key={index}
