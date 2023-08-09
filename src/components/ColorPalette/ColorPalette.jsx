7e10c655ca0668c4b1c89afdd3d4626523ee5719
biancashiromoto <b.shiromoto.bs@gmail.com>
2023-08-09 20:45:31 -0300

    feat: Add getNewColorPalette function

--- src/components/ColorPalette/ColorPalette.jsx
+++ src/components/ColorPalette/ColorPalette.jsx
@@ -4,11 +4,27 @@ import Pixel from '../Pixel/Pixel';
 import './ColorPalette.css';
 
 const ColorPalette = () => {
-  const { colors, setCurrColor } = useContext(context);
+  const { setCurrColor, paletteColors, setPaletteColors } = useContext(context);
+
+  const calcRandomColor = () => {
+    const red = Math.floor(Math.random() * 256);
+    const green = Math.floor(Math.random() * 256);
+    const blue = Math.floor(Math.random() * 256);
+    return `rgb(${red}, ${green}, ${blue})`;
+  };
+
+  const getNewColorPalette = () => {
+    let newPalette = [];
+    paletteColors.map(() => {
+      const newColor = calcRandomColor();
+      newPalette.push(newColor);
+    })
+    setPaletteColors(newPalette);
+  };
 
   return (
     <div className='d-flex mx-2'>
-      {colors.map((color, index) => (
+      {paletteColors.map((color, index) => (
         <div key={index} className='pixel-container d-flex'>
             <Pixel
               color={color}
@@ -16,6 +32,9 @@ const ColorPalette = () => {
             />
         </div>
       ))}
+      <button
+        onClick={ () => getNewColorPalette() }
+      >Random colors</button>
     </div>
   )
 }
