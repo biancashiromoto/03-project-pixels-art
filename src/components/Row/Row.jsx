01471e01a32388398b57ff9ffdab23570b9e0735
biancashiromoto <b.shiromoto.bs@gmail.com>
2023-08-09 19:33:49 -0300

    refactor: Change white format to rbg

--- src/components/Row/Row.jsx
+++ src/components/Row/Row.jsx
@@ -1,19 +1,21 @@
 import { useContext } from "react";
 import { context } from "../../context/context";
 import Pixel from '../Pixel/Pixel';
+import './Row.module.css';
 
 const Row = () => {
   const { currColor, pixels } = useContext(context);
 
   const paintPixel = (e) => {
     let pixelStyle = e.target.style;
-    (pixelStyle.backgroundColor === 'white')
+    const white = 'rgb(255, 255, 255)';
+    (pixelStyle.backgroundColor === white)
       ? (pixelStyle.backgroundColor = currColor)
-      : (pixelStyle.backgroundColor = 'white');
+      : (pixelStyle.backgroundColor = white);
   }
 
   return (
-    <div className='d-flex'>
+    <div className='pixel-container d-flex'>
       {pixels.map((_pixel, index) => (
         <Pixel
           key={index}
