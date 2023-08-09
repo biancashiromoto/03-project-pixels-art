c21c16a698bae4366a23497404bcddafb72ef085
biancashiromoto <b.shiromoto.bs@gmail.com>
2023-08-09 19:26:45 -0300

    feat: Import BoardSizeSelector

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Provider from './context/Provider';
 import Board from './components/Board/Board';
 import ColorPalette from './components/ColorPalette/ColorPalette';
+import BoardSizeSelector from './components/BoardSizeSelector/BoardSizeSelector';
 
 function App() {
   return (
    <Provider>
     <h1>Pixels Art</h1>
       <ColorPalette />
+      <BoardSizeSelector />
       <Board />
   </Provider>
   )
