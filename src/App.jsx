7a6f1201a8d7cba8e3b90efa8c5d87084cbb54da
biancashiromoto <b.shiromoto.bs@gmail.com>
2023-08-09 18:24:33 -0300

    feat: Add ColorPalette component to App.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Provider from './context/Provider';
-import Board from './components/Board/Board';
+import ColorPalette from './components/ColorPalette/ColorPalette';
 
 function App() {
   return (
    <Provider>
     <h1>Pixels Art</h1>
-      <Board />
+      <ColorPalette />
   </Provider>
   )
 }
