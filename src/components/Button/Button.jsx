66809e2ef315b936db65f07a3461dbe914a0546a
biancashiromoto <b.shiromoto.bs@gmail.com>
2023-08-10 18:08:02 -0300

    refactor: Replace button for Button component

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,20 @@
+import PropTypes from "prop-types";
+
+const Button = ({label, onClick, className}) => {
+  return (
+    <button
+      className={className}
+      onClick={onClick}
+    >
+      {label}
+    </button>
+  )
+}
+
+Button.propTypes = {
+  className: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired,
+  onClick: PropTypes.func.isRequired
+}
+
+export default Button;
