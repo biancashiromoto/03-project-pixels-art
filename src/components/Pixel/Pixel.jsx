c5b5ea26e37de854fd8d5ac6182816a31713fe62
biancashiromoto <b.shiromoto.bs@gmail.com>
2023-08-10 17:37:39 -0300

    feat: Add logic to define pixels size

--- src/components/Pixel/Pixel.jsx
+++ src/components/Pixel/Pixel.jsx
@@ -1,20 +1,26 @@
 import PropTypes from "prop-types"
 import './Pixel.css';
 
-const Pixel = ({ onClick, color }) => {
+const Pixel = ({ onClick, color, width }) => {
+  const widthValue = Math.floor(width);
   return (
     <div
         className={ `pixel border border-dark` }
         onClick={ onClick }
-        style={ { backgroundColor: color } }
+        style={ {
+          backgroundColor: color,
+          width: `${(widthValue)}px`,
+          height: `${(widthValue)}px`,
+        } }
     >
     </div>
   )
 }
 
 Pixel.propTypes = {
-  color: PropTypes.string,
-  onClick: PropTypes.func.isRequired
+  color: PropTypes.string.isRequired,
+  onClick: PropTypes.func.isRequired,
+  width: PropTypes.number
 }
 
 export default Pixel;
