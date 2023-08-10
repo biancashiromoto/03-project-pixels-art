c280af27a3edcd4b5d8dfeff336050734bef7c68
biancashiromoto <b.shiromoto.bs@gmail.com>
2023-08-10 18:18:34 -0300

    refactor: Adjust Row component and import helpers

--- src/components/Row/Row.jsx
+++ src/components/Row/Row.jsx
@@ -1,37 +1,27 @@
 import { useContext, useEffect, useState } from "react";
 import { context } from "../../context/context";
 import Pixel from '../Pixel/Pixel';
-import './Row.css';
+import { paintPixel } from "../../helpers/paintPixel";
+import { resizePixel } from "../../helpers/resizePixel";
+import { createArray } from "../../helpers/createArray";
+import { INITIAL_PIXEL_SIZE } from "../../helpers/info";
 
 const Row = () => {
-  const { currColor, pixels, boardSize } = useContext(context);
-  const [pixelSize, setPixelSize] = useState(100);
-
-  const paintPixel = (e) => {
-    let pixelStyle = e.target.style;
-    const white = 'rgb(255, 255, 255)';
-    (pixelStyle.backgroundColor === currColor)
-      ? (pixelStyle.backgroundColor = white)
-      : (pixelStyle.backgroundColor = currColor);
-  }
+  const { currColor, boardSize } = useContext(context);
+  const [pixelSize, setPixelSize] = useState(INITIAL_PIXEL_SIZE);
+  const pixels = createArray(boardSize);
 
   useEffect(() => {
-    const newPixelSize = 100 - (boardSize * 7.5);
-    const minPixelSize = 30;
-    const currPixelSize = Math.max(newPixelSize, minPixelSize);
+    const currPixelSize = resizePixel(boardSize);
     setPixelSize(currPixelSize);
   }, [boardSize]);
-
-  useEffect(() => {
-    console.log(pixelSize);
-  }, [pixelSize]);
 
   return (
     <div className={ `row d-flex pixel-container` }>
       {pixels.map((_pixel, index) => (
         <Pixel
           key={index}
-          onClick={ (e) => paintPixel(e) }
+          onClick={ (e) => paintPixel(e, currColor) }
           width={ `${pixelSize}` }
         />
       ))}
