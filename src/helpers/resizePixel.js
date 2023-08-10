529553f6e882cb6a57a1d83f150019a8505ee720
biancashiromoto <b.shiromoto.bs@gmail.com>
2023-08-10 19:14:17 -0300

    chore: Set Board components width depending on screenWidth

--- src/helpers/resizePixel.js
+++ src/helpers/resizePixel.js
@@ -2,5 +2,5 @@ import { getScreenWidth } from "./getScreenWidth";
 
 export const resizePixel = (boardSize) => {
   const screenWidth = getScreenWidth();
-    return screenWidth / boardSize;
+    return (screenWidth - (screenWidth * 0.2)) / boardSize;
 }
