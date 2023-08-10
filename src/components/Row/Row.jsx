81c63c66c59bae6d87a75138eb549af7d91e51ea
biancashiromoto <b.shiromoto.bs@gmail.com>
2023-08-10 17:39:24 -0300

    feat: Set up logic to resize pixels depending on boardSize

--- src/components/Row/Row.jsx
+++ src/components/Row/Row.jsx
@@ -1,9 +1,11 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { context } from "../../context/context";
 import Pixel from '../Pixel/Pixel';
+import './Row.css';
 
 const Row = () => {
-  const { currColor, pixels } = useContext(context);
+  const { currColor, pixels, boardSize } = useContext(context);
+  const [pixelSize, setPixelSize] = useState(100);
 
   const paintPixel = (e) => {
     let pixelStyle = e.target.style;
@@ -13,12 +15,24 @@ const Row = () => {
       : (pixelStyle.backgroundColor = currColor);
   }
 
+  useEffect(() => {
+    const newPixelSize = 100 - (boardSize * 7.5);
+    const minPixelSize = 30;
+    const currPixelSize = Math.max(newPixelSize, minPixelSize);
+    setPixelSize(currPixelSize);
+  }, [boardSize]);
+
+  useEffect(() => {
+    console.log(pixelSize);
+  }, [pixelSize]);
+
   return (
-    <div className={ `d-flex pixel-container}` }>
+    <div className={ `row d-flex pixel-container` }>
       {pixels.map((_pixel, index) => (
         <Pixel
           key={index}
           onClick={ (e) => paintPixel(e) }
+          width={ `${pixelSize}` }
         />
       ))}
     </div>
