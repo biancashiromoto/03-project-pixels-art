c9ccdfe7200d744cb723a8028e6604b0897e242f
biancashiromoto <b.shiromoto.bs@gmail.com>
2023-08-10 18:08:33 -0300

    chore: Create info.js to store variables

---
+++ src/helpers/info.js
@@ -0,0 +1,13 @@
+export const INITIAL_BOARD_SIZE = 4;
+
+export const MAX_BOARD_SIZE = 10;
+
+export const colors = [
+  '#985BEB',
+  '#EB8B67',
+  '#4F8BEA',
+  '#F0EC62',
+  '#83FCE6'
+];
+
+export const randomColorsButton = 'Change colors';
