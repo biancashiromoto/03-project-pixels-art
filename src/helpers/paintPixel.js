e8f34672d0ca731cd59be683c183f6c7656168a3
biancashiromoto <b.shiromoto.bs@gmail.com>
2023-08-10 18:40:54 -0300

    chore: Delete paintPixel.js

--- src/helpers/paintPixel.js
+++
@@ -1,7 +0,0 @@
-export const paintPixel = (e, currColor) => {
-  let pixelStyle = e.target.style;
-  const white = 'rgb(255, 255, 255)';
-  (pixelStyle.backgroundColor === currColor)
-    ? (pixelStyle.backgroundColor = white)
-    : (pixelStyle.backgroundColor = currColor);
-};
