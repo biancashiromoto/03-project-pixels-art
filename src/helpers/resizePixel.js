3143921b707d7718e2ec5c3be226f9521974ab93
biancashiromoto <b.shiromoto.bs@gmail.com>
2023-08-11 19:51:18 -0300

    refactor: Create conditions to new pixel size

--- src/helpers/resizePixel.js
+++ src/helpers/resizePixel.js
@@ -1,6 +1,16 @@
 import { getScreenWidth } from "./getScreenWidth";
 
+const isMobileScreen = (value) => value <= 320;
+const isTabletScreen = (value) => value > 321 && value <= 768;
+const isMonitorScreen = (value) => value > 768;
+
 export const resizePixel = (boardSize) => {
   const screenWidth = getScreenWidth();
-    return (screenWidth - (screenWidth * 0.25)) / boardSize;
+  if (isMobileScreen(screenWidth)) {
+    return (screenWidth - (screenWidth * .25)) / boardSize;
+  } 
+  if (isTabletScreen(screenWidth)) {
+    return (screenWidth - (screenWidth * 0.35)) / boardSize;
+  }
+  return isMonitorScreen(screenWidth) && (screenWidth - (screenWidth * 0.75)) / boardSize;
 }
