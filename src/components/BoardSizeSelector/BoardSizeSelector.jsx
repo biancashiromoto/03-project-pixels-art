e367318c041d12765316cf74c9230eb92980b865
biancashiromoto <b.shiromoto.bs@gmail.com>
2023-08-11 17:43:39 -0300

    feat: Add control buttons for boardSize

--- src/components/BoardSizeSelector/BoardSizeSelector.jsx
+++ src/components/BoardSizeSelector/BoardSizeSelector.jsx
@@ -1,6 +1,7 @@
 import { useContext } from 'react';
 import { context } from '../../context/context';
 import { INITIAL_BOARD_SIZE, MAX_BOARD_SIZE } from '../../helpers/info';
+import Button from '../Button/Button';
 
 const BoardSizeSelector = () => {
   const { boardSize, setBoardSize } = useContext(context);
@@ -11,17 +12,36 @@ const BoardSizeSelector = () => {
   };
 
   return (
-    <form className='form-group'>
-      <input
-        className='form-control-range'
-        id='formControlRange'
-        min={INITIAL_BOARD_SIZE}
-        max={MAX_BOARD_SIZE}
-        type='range'
-        defaultValue={INITIAL_BOARD_SIZE}
-        onChange={ (e) => handleInputChange(e) }
+    <form className='form-group d-flex align-items-center justify-content-center'>
+      <Button
+        onClick={(e)=> {
+          e.preventDefault();
+          console.log(boardSize);
+          setBoardSize(prevSize => prevSize <= INITIAL_BOARD_SIZE ? INITIAL_BOARD_SIZE : prevSize - 1)
+        }}
+        label='-'
       />
-      <span>{boardSize}</span>
+     <div className='d-flex flex-column align-items-center justify-content-center'>
+      <input
+          className='form-control-range'
+          id='formControlRange'
+          min={INITIAL_BOARD_SIZE}
+          max={MAX_BOARD_SIZE}
+          type='range'
+          defaultValue={INITIAL_BOARD_SIZE}
+          value={boardSize}
+          onChange={ (e) => handleInputChange(e) }
+          style={ {width: '200px'}}
+        />
+        <span>{boardSize}</span>
+     </div>
+      <Button
+        onClick={(e)=> {
+          e.preventDefault();
+          setBoardSize(prevSize => prevSize >= MAX_BOARD_SIZE ? MAX_BOARD_SIZE : prevSize + 1)
+        }}
+        label='+'
+      />
     </form>
   )
 }
